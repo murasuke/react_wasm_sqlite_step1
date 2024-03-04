bcba9a1b6ebaea96a8dbfc1dff60d2683d00642d
tkyk.niimura <tkyk.niimura@gmai.com>
2024-03-04 20:28:43 +0900

    add github pages setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? 'react_wasm_sqlite_step1' : './',
   plugins: [react()],
   server: {
     headers: {
